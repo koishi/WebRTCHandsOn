7819cee5bfc7a2a775c866339208093a2f995109
Koichiro Oishi <bs@v7.com>
2017-06-18 12:15:33 +0900

    add some view

---
+++ WebRTCHandsOn/ChatViewController.swift
@@ -0,0 +1,39 @@
+//
+//  ChatViewController.swift
+//  WebRTCHandsOn
+//
+//  Created by 大石弘一郎 on 2017/06/18.
+//  Copyright © 2017年 Koichiro Oishi. All rights reserved.
+//
+
+import UIKit
+import WebRTC
+
+class ChatViewController: UIViewController {
+
+    @IBOutlet weak var videoView: RTCEAGLVideoView!
+    @IBOutlet weak var cameraPreviewView: RTCCameraPreviewView!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ WebRTCHandsOn/RTCCameraPreviewView.swift
@@ -0,0 +1,21 @@
+//
+//  RTCCameraPreviewView.swift
+//  WebRTCHandsOn
+//
+//  Created by 大石弘一郎 on 2017/06/18.
+//  Copyright © 2017年 Koichiro Oishi. All rights reserved.
+//
+
+import UIKit
+
+class RTCCameraPreviewView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
